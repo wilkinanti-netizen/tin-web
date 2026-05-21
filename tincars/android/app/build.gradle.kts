ffd36de9bfb417ab098d69da937c5043557546d2
wilkinGPT <wilkinbalmacee321@gmail.com>
2026-05-21 10:23:09 -0500

    Configure release signing configuration and update version to 1.0.1+4

--- tincars/android/app/build.gradle.kts
+++ tincars/android/app/build.gradle.kts
@@ -32,11 +32,20 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("../../keystore.jks")
+            storePassword = "MySuperSecretPassword123!"
+            keyAlias = "tincars_key"
+            keyPassword = "MySuperSecretPassword123!"
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
